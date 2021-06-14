dcb2bf95f9005571e3309b0243726333219331e8
Andrey <dyxa1@mail.ru>
2021-06-14 20:13:08 +0300

    Added Task3

---
+++ src/Task3.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
+        int lastNum = array[0];
+        array[0] =  array[array.length-1];
+        array[array.length-1] = lastNum;
+        System.out.println("Результат суммы первого и среднего элемента: " + (array[0] + array[(array.length-1)/2]));
+    }
+}
