df87df3f9bae86d812e6bdae20402a1e00e6a797
Andrey <dyxa1@mail.ru>
2021-06-14 16:49:38 +0300

    Added Task2

---
+++ src/Task2.java
@@ -0,0 +1,9 @@
+public class Task2 {
+
+    public static void main(String[] args){
+        double num1 = 0.78;
+        final byte num2 = 127;
+        System.out.println("Произвольное дробное значение переменной типа с плавающей точкой с наибольшим диапазоном чисел: " + num1);
+        System.out.println("Наибольшее возможное значение переменной целочисленного типа с наименьшим диапазоном чисел: " + num2);
+    }
+}
