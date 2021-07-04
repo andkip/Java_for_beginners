bdbcbab787e26242b681dfedfb9d5d2ce65e0957
Andrey <dyxa1@mail.ru>
2021-07-04 19:23:52 +0300

    Fix Task11

--- src/Task11.java
+++ src/Task11.java
@@ -5,10 +5,10 @@ public class Task11 {
     public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     System.out.println("Введите первое число:");
-    String firstNum = scan.next();
+    String firstNum = Float.toString(scan.nextFloat());
     System.out.println("Введите второе число:");
     int secondNum = scan.nextInt();
-    int a = Integer.parseInt(firstNum);
+    float a = Float.parseFloat(firstNum);
     System.out.println("Большее число: " + Math.max(a,secondNum));
     double minNum = Math.min(a,secondNum);
     System.out.println("Меньшее число: " + minNum);
